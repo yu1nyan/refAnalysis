9b10d1bb827ffd2af056b2ddb6aa5165ea2c74f2
awayui <yuiyuiawayui@gmail.com>
2019-12-05 06:31:57 +0900

    一通り見れるようにした

--- const.h
+++ const.h
@@ -1,45 +1,14 @@
-const int NChZ = 9;
-const int NChZAround = 8;
-const string histNameZNumberCenter = "11";
-const string histNameZNumberAround[] = {"02", "12", "22", "01", "21", "00", "10", "20"};
-const string histNameZNPE = "npz";
-// const string histNameZ[] = { "npz02", "npz12", "npz22", "npz01", "npz11", "npz21", "npz00", "npz10", "npz20" };
-const string histNameZAround[] = { "npz02", "npz12", "npz22", "npz01", "npz21", "npz00", "npz10", "npz20" };
-const string histNameZCenter = "npz11";
-// const string CubeGeometryName[] = { "UpperLeft", "Upper", "UpperRight", "Left", "Center", "Right", "LowerLeft", "Lower", "LowerRight" };
-const string CubeGeometryNameAround[] = { "UpperLeft", "Upper", "UpperRight", "Left", "Right", "LowerLeft", "Lower", "LowerRight" };
-// const string CubeGeometryNameCenter = "Center";
-// const string CubeGeometryTitle[] = { "upper left", "upper", "upper right", "left", "center", "right", "lower left", "lower", "lower right" };
-const string CubeGeometryTitleAround[] = { "upper left", "upper", "upper right", "left", "right", "lower left", "lower", "lower right" };
-// const string CubeGeometryTitleCenter = "center";
+enum class RunMode {WithoutRef, WithRef};
 
-const string histNameZHitTime = "hittimez";
+const double ThresholdRef = 20;   // Refありなしを判定するためのしきい値 (pe)
+const int FitRangeGausLeft = 10;
+const int FitRangeGausRight = 10;
 
-const string histNameCubeIn = "cubeinpos";
-const string histNameCubeOut = "cubeoutpos";
 
-const string histName2ndParticleFromCenter = "2ndFromCenterTo";
-const string histName2ndParticleFromHodo = "2ndFromHodoTo";
+const int UsingCh = 10;
+const int NRebin = 32;
+const int OverFlowCut = 4085;
 
-const string histNameCoord[] = {"x", "y", "z"};
-
-const double MinPECenter = -1.5;
-const double MaxPECenter = 108.5;
-const int NBinPECenter = 110;
-const double MinPEAround = -0.5;
-const double MaxPEAround = 9.5;
-const int NBinPEAround = 10;
-const double MinCT = -0.1;
-const double MaxCT = 1.0;
-const double NBinCT = 110;
-const double MinCTCellMap = 0.0;
-const double MaxCTCellMap = 10.0;
-
-const double CubeEdgeXY1 = -5.0; // mm
-const double CubeEdgeXY2 = 5.0;
-const double OmitXY1 = -3.4;    //mm
-const double OmitXY2 = 3.4;
-// const double CubeEdgeY1 = -5.0;
-// const double CubeEdgeY2 = 5.0;
-const double CubeEdgeZup = 5.0;
-const double CubeEdgeZdown = -5.0;
+const double AdcPedestal = 223.8;
+const double Adc1pe = 273.8;
+const double Gain = Adc1pe - AdcPedestal;
--- refAnalysis.cc
+++ refAnalysis.cc
@@ -43,12 +43,6 @@ using namespace std;
 
 
 
-
-tuple<int, int> ConvertCellPosition(int cellX, int cellY)
-{
-    return forward_as_tuple(cellX + 1, cellY + 1);
-}
-
 void SaveHist(TH1* hist, TString outputFileDir, TString drawOption = "", bool setLogy = false, int histWidth = 0, int histHeight = 0)
 {
     TCanvas* canvas;
@@ -70,32 +64,38 @@ void SaveHist(TH1* hist, TString outputFileDir, TString drawOption = "", bool se
     canvas->Clear();
 }
 
-void SaveHodoMap(TH2* hist, TString outputFileDir, int nCellOneSide)
+void changeStatsBoxSize(TH1* hist, double x1, double x2, double y1, double y2)
 {
-    TCanvas* canvas = new TCanvas("canvas", "", 1280, 1200);
-    hist->Draw("text colz");
-    hist->GetXaxis()->SetNdivisions(nCellOneSide);
-    hist->GetYaxis()->SetNdivisions(nCellOneSide);
-    gPad->SetRightMargin(0.17);
-    hist->GetZaxis()->SetTitleOffset(1.4);
-    hist->SetStats(kFALSE);
-    canvas->SaveAs(outputFileDir);
-    canvas->Clear();
+    gPad->Update();
+    TPaveStats* st = (TPaveStats*) hist->FindObject("stats");
+    st->SetX1NDC(x1);
+    st->SetX2NDC(x2);
+    st->SetY1NDC(y1);
+    st->SetY2NDC(y2);
+    st->Draw();
 }
 
-void SaveGraph(TGraph* graph, TString outputFileDir)
+void changeOptionStat(TH1* hist, int option)
 {
-    TCanvas* canvas = new TCanvas();
-    graph->Draw("AP");
-    canvas->SaveAs(outputFileDir);
-    canvas->Clear();
+    gPad->Update();
+    TPaveStats* st = (TPaveStats*) hist->FindObject("stats");
+    st->SetOptStat(option);
+    st->Draw();
+}
+
+void changeOptionFit(TH1* hist, int option)
+{
+    gPad->Update();
+    TPaveStats* st = (TPaveStats*) hist->FindObject("stats");
+    st->SetOptFit(option);
+    st->Draw();
 }
 
 void refAnalysis(string rootFileDirectory, string outputFileType = "png")
 {
     // Make result directories
-    // const string EachCellDir = rootFileDirectory + "/crosstalkEachCell/";
-    // mkdir(EachCellDir.c_str(), 0777);
+    const string ResultDir = rootFileDirectory + "/result/";
+    mkdir(ResultDir.c_str(), 0777);
 
 
     // ROOTファイル名の取得
@@ -119,7 +119,19 @@ void refAnalysis(string rootFileDirectory, string outputFileType = "png")
         closedir(dp);
     }
 
-    // int allevt = 0; // scatter plot 用
+    const double HistMin = -9.5;
+    const double HistMax = 90.5;
+    const int NBin = 100;
+    TH1D* hAdcValue = new TH1D("hAdcValue", "ADC Value (ch10);ADC Value;Number of events", 500, 0, 500);
+    TH1D* hist = new TH1D("hist", "Light intensity (ch10);Light intensity (p.e.);Number of events", NBin, HistMin, HistMax);
+
+
+
+    vector <int> adcValues;
+    int evt = 0;
+    int maxAdcValue = 0;
+    int maxEvents = 0;
+
     for (string rootFileName : rootFileNames)
     {
         // ファイルオープン・ツリー取得
@@ -133,23 +145,65 @@ void refAnalysis(string rootFileDirectory, string outputFileType = "png")
         int NEntries = tree->GetEntries();
         cout << "Number of events: " << NEntries << endl;
         tree->SetBranchAddress("chg", adc);
-        TH1D* hist = new TH1D("hist", "hist", 100, 0, 4000);
+
         for (int i = 0; i < NEntries; i++)
         {
             tree->GetEntry(i);
-            // cout << adc[10] << endl;
-            if(adc[10] >= 4085)
-            {
+            // if(maxAdcValue < adc[UsingCh])
+            // {
+            //     maxEvents = evt;
+            //     maxAdcValue = adc[UsingCh];
+            // }
+            // ++evt;
+            // adcValues.push_back(adc[UsingCh]);
+            if(adc[UsingCh] > OverFlowCut)
                 continue;
-            }
-            hist->Fill(adc[10]);
-        }
-        hist->Draw("E");
 
-        TString outputFileDir = TString::Format("%s/ch10.%s", rootFileDirectory.c_str(), outputFileType.c_str());
-        SaveHist(hist, outputFileDir);
+            double pe = (adc[UsingCh] - AdcPedestal) / Gain;
+            // cout << "pe = " << pe << endl;
+            hAdcValue->Fill(adc[UsingCh]);
+            hist->Fill(pe);
+        }
     }
 
+
+
+    double maxBin = hist->GetMaximumBin() + HistMin;
+    cout << maxBin << endl;
+    RunMode runMode;
+    if(maxBin < ThresholdRef)
+    {
+        runMode = RunMode::WithRef;
+        // TF1* poissonInt = new TF1("poissonInt", "[0]*TMath::PoissonI(x, [1])", -0.5, 5.5);
+        hist->Fit("gaus", "", "", maxBin-3, maxBin+5);
+        changeStatsBoxSize(hist, 0.7, 0.98, 0.6, 0.95);
+        changeOptionStat(hist, 10);
+        changeOptionFit(hist, 110);
+    }
+
+    else
+    {
+        runMode = RunMode::WithoutRef;
+        // hist->Rebin(NRebin);
+        hist->Fit("gaus", "", "", maxBin-FitRangeGausLeft, maxBin+FitRangeGausRight);
+        changeStatsBoxSize(hist, 0.15, 0.4, 0.6, 0.95);
+        changeOptionStat(hist, 10);
+        changeOptionFit(hist, 110);
+    }
+
+
+
+
+
+    TString outputFileDir = TString::Format("%s/ch10_calibrated.%s", ResultDir.c_str(), outputFileType.c_str());
+
+    TFile histFile(TString::Format("%shist.root", ResultDir.c_str()), "RECREATE");
+    SaveHist(hist, outputFileDir);
+    hist->Write();
+    histFile.Close();
+
+    outputFileDir = TString::Format("%sch10_raw.%s", ResultDir.c_str(), outputFileType.c_str());
+    SaveHist(hAdcValue, outputFileDir);
 
 }
 
